03fae624f02b50f25f01fe1ffc65a3e94ed5ea5c
Alyaesub <reynierpascal@yahoo.com>
2025-01-20 21:49:18 +0100

    change sass score on player section

--- script/script.js
+++ script/script.js
@@ -6,24 +6,19 @@ const rollButton = document.getElementById("rollDice"); // Bouton pour lancer le
 const diceFace = document.querySelector(".rolling-element"); // Image du dé
 const diceResult = document.getElementById("diceResult"); // Affiche le resultat du dés
 
+// Fonction pour lancer le dé
 function rollDice() {
-	// Fonction pour lancer le dé
-
 	// Réinitialise l'image du dé a chaque lancement
 	diceFace.src = "assets/game-die.256x256-removebg-preview.png";
 	diceResult.textContent = "Le dé roule...";
-
 	// Ajoute une animation temporaire
 	diceFace.classList.add("rolling"); // Ajoute une classe pour déclencher l'animation
-
 	// Supprime l'animation après 2 secondes (durée de l'animation définie en CSS)
 	setTimeout(() => {
 		diceFace.classList.remove("rolling");
 		const result = Math.floor(Math.random() * 6) + 1; //variable qui calcul le resultat random
-
 		// Mettre à jour le résultat visuel du dé
 		diceFace.src = `assets/die-face-${result}.png`;
-
 		// Afficher le résultat sous le dé
 		diceResult.textContent = `Vous avez obtenu un ${result} !`;
 	}, 2000); //2 secondes
@@ -32,3 +27,5 @@ function rollDice() {
 rollButton.addEventListener("click", rollDice);
 
 //////////////////////////// FIN DE L'ANIMATION /////////////////////////////
+
+///////////////////// Fonction pour me changement de joueur ////////////////
