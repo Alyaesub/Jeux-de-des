42438005a2b3052650de65067b1a59e999e72da0
Alyaesub <reynierpascal@yahoo.com>
2025-02-08 18:53:40 +0100

    add player selection and roundscor jq

--- script/script.js
+++ script/script.js
@@ -1,6 +1,12 @@
 console.log("teste réussi");
 
-/////////////////////// ANIMATION DU DES //////////////////////////////////
+//////////////////////////////// Variables globales /////////////////////////////////
+// Variables pour suivre les scores
+let totalScore = 0;
+let roundScore = 0;
+//-----------------------------------------------------------------------------------------//
+
+////////////////////////////// ANIMATION DU DES //////////////////////////////////
 
 const rollButton = document.getElementById("rollDice"); // Bouton pour lancer le dé
 const diceFace = document.querySelector(".rolling-element"); // Image du dé
@@ -18,14 +24,30 @@ function rollDice() {
 		diceFace.classList.remove("rolling");
 		const result = Math.floor(Math.random() * 6) + 1; //variable qui calcul le resultat random
 		// Mettre à jour le résultat visuel du dé
+
 		diceFace.src = `assets/die-face-${result}.png`;
 		// Afficher le résultat sous le dé
-		diceResult.textContent = `Vous avez obtenu un ${result} !`;
+		diceResult.textContent = `Vous avez obtenu un ${result} ! `;
+
+		//logique de resultat
+		if (result === 1) {
+			//si 1 score reset
+			roundScore = 0;
+			diceResult.textContent += "Oups raté t'est nul!!!!!";
+		} else {
+			roundScore += result;
+			document.querySelector(
+				".player.active .round-score"
+			).textContent = ` Score du Tour : ${roundScore} `;
+		}
 	}, 2000); //2 secondes
+	//algo if else a mettre ici pour le 1 et le scor du round
 }
 // Associe la fonction au clic sur le bouton
 rollButton.addEventListener("click", rollDice);
 
-//////////////////////////// FIN DE L'ANIMATION /////////////////////////////
+//----------------------------------------------------------------------------------------//
 
-///////////////////// Fonction pour me changement de joueur ////////////////
+////////////////////////// Fonction pour le changement de joueur /////////////////////
+
+//-------------------------------------------------------------------------------//
